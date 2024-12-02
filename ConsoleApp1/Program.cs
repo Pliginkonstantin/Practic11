ff6f17ea78045a9153477f0e73828a776ffd08ec
Pliginkonstantin <falsekiller3@gmail.com>
2024-12-02 21:39:55 +0200

    Task1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,10 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        /// <summary>
+        /// определить статическим методом int Centimeters (int m , int c), который принимает два целых аргумента (метры m , сантиметры c) и возвращает количество сантиметров 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             int m = InputIntNumber("метры      : ");
@@ -16,15 +20,33 @@ namespace ConsoleApp1
             OutPutInt(Centimeters(m, c));
             Console.Read();
         }
+        /// <summary>
+        /// вычисление сантиметров из метров с добавлением сантиметров
+        /// </summary>
+        /// <param name="m">int</param>
+        /// <param name="c">int</param>
+        /// <returns>int</returns>
         static int Centimeters(int m,  int c)
         {
             return m * 100 + c;
         }
+        /// <summary>
+        /// ввод целого значения
+        /// </summary>
+        /// <param name="message">int</param>
+        /// <returns>int</returns>
         static int InputIntNumber(string message)
         {
             Console.Write(message);
             return int.Parse(Console.ReadLine());
         }
-        static void OutPutInt(int message) { Console.WriteLine($"всего      : {message} см"); }
+        /// <summary>
+        /// вывод значений
+        /// </summary>
+        /// <param name="message">int</param>
+        static void OutPutInt(int message) 
+        { 
+            Console.WriteLine($"всего      : {message} см");
+        }
     }
 }
