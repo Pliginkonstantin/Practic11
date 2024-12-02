56565a558a99856b56875333666b9bc49140eb4f
Pliginkonstantin <falsekiller3@gmail.com>
2024-12-02 21:13:46 +0200

    Task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = InputIntNumber("метры      : ");
+            int c = InputIntNumber("сантиметры : ");
+            Console.WriteLine("==========================");
+            OutPutInt(Centimeters(m, c));
+            Console.Read();
+        }
+        static int Centimeters(int m,  int c)
+        {
+            return m * 100 + c;
+        }
+        static int InputIntNumber(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        static void OutPutInt(int message) { Console.WriteLine($"всего      : {message} см"); }
+    }
+}
